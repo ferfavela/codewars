ee6d03e5a9db24495aa6b7859d095166c2dcd5fc
Fernando Favela <102422689+ferfavela@users.noreply.github.com>
2022-04-14 11:49:51 -0300

    Add files via upload

---
+++ Convert_a_Boolean_to_a_String.py
@@ -0,0 +1,9 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+def bool_to_str(b):
+    if b:
+        return "True"
+    else:
+        return "False"
