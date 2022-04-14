425675f7fd5dfd371a6dffc208aa1bb625725e08
Fernando Favela <102422689+ferfavela@users.noreply.github.com>
2022-04-14 11:50:47 -0300

    Add files via upload

---
+++ Drink_about.py
@@ -0,0 +1,22 @@
+# Kids drink toddy.
+# Teens drink coke.
+# Young adults drink beer.
+# Adults drink whisky.
+# Make a function that receive age, and return what they drink.
+
+# Rules:
+
+# Children under 14 old.
+# Teens under 18 old.
+# Young under 21 old.
+# Adults have 21 or more.
+
+def age_drink(age):
+    if age < 14:
+        return "drink toddy"
+    elif age >= 14 and age < 18:
+        return "drink coke"
+    elif age >= 18 and age < 21:
+        return "drink beer"
+    else:
+        return "drink whisky"
